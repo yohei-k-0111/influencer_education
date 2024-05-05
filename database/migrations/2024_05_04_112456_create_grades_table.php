0b4d9ce4ea06b70481d21dcc8d589dd9764c7bba
yohei-k-0111 <y.kusumoto727@gmail.com>
2024-05-05 16:58:49 +0900

    マイグレーションファイル追加_20240505

--- database/migrations/2024_05_04_112456_create_grades_table.php
+++ database/migrations/2024_05_04_112456_create_grades_table.php
@@ -13,14 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::create('grades', function (Blueprint $table) {
+            $table->increments('id'); // bigint(10) の学年id
+            $table->string('name', 255); // varchar(255) の学年名 notnull
+            $table->timestamps(); // 作成日時created_at と 更新日時updated_at のtimestamp
+
         });
     }
 
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('grades');
     }
 };
---
+++ database/migrations/2024_05_04_112501_create_classes_clear_checks_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes_clear_checks', function (Blueprint $table) {
+            $table->increments('id'); // bigint(10) のid
+            $table->unsignedInteger('users_id'); // 符号なしbigint(10) のユーザーid notnull
+            $table->unsignedInteger('grade_id'); // 符号なしbigint(10) の学年id notnull
+            $table->tinyInteger('clear_flg'); // tinyint(4) 1:クリア、0:未クリア
+            // curriculumsテーブルにある同一クラスIDのカリキュラムがすべてcurriculum_progressテーブルにてclear_flg =1になった際、このカラムも1にする。
+            $table->timestamps();
+
+            // 外部キー制約を追加
+            $table->foreign('users_id')
+                  ->references('id')
+                  ->on('users')
+                  ->cascadeOnDelete()  // ON DELETE で CASCADE
+                  ->cascadeOnUpdate(); // ON UPDATE で CASCADE
+
+            $table->foreign('grade_id')
+                  ->references('id')
+                  ->on('grades')
+                  ->cascadeOnDelete()  // ON DELETE で CASCADE
+                  ->cascadeOnUpdate(); // ON UPDATE で CASCADE
+
+      
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes_clear_checks');
+    }
+};
