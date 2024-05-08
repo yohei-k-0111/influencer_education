45d02fefc6ddbc448806012e1f88c45e2e6c79df
yohei-k-0111 <y.kusumoto727@gmail.com>
2024-05-08 17:04:16 +0900

    マイグレーションファイル追加_20240508

---
+++ database/migrations/2024_05_04_112456_create_classes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('classes', function (Blueprint $table) {
+            $table->increments('id'); // bigint(10) の学年id
+            $table->string('name', 255); // varchar(255) の学年名 notnull
+            $table->timestamps(); // 作成日時created_at と 更新日時updated_at のtimestamp
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('classes');
+    }
+};
